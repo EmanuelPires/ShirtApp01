abe15a8f7c52f5e1991cbd836e27d64a92851b6c
Emanuel Pires <emanuelpires11@gmail.com>
2023-03-23 09:21:03 -0600

    canvas resizing but problems with proportions

--- shirtapp01/src/App.js
+++ shirtapp01/src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShirtTest1 from "./pages/ShirtTest1";
 
 export default function App() {
-  return (<ShirtTest1/>);
+  return <ShirtTest1 />;
 }
--- shirtapp01/src/pages/shirtTest1.js
+++ shirtapp01/src/pages/shirtTest1.js
@@ -1,29 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 export default function ShirtTest1() {
-  console.log("Hey Now!");
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-  
-  useEffect(()=>{
-    function handleWidth(){ 
-      setViewportWidth(window.innerWidth)
+  let canvasWidth = viewportWidth * 0.6;
+  let canvasHeight = canvasWidth * 0.3;
+
+  //const leftSideRef = useRef();
+  //console.log(leftSideRef.current + "left Ref");
+  //console.log(leftDivWidth.toString() + "left Div Width");
+
+  // useLayoutEffect(() => {
+  //   if (leftSideRef.current) {
+  //     setLeftDivWidth(leftSideRef.current.offsetWidth);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    function handleWidth() {
+      setViewportWidth(window.innerWidth);
+      console.log("Window size is " + viewportWidth);
+      console.log("canvas width is " + canvasWidth);
+      console.log("canvas height is " + canvasHeight);
     }
-  window.addEventListener("resize", handleWidth)
-  })
-  console.log(viewportWidth+" This is the viewportwidth variable")
-  console.log(setViewportWidth+" This is the set viewportwidth variable" )
+    window.addEventListener("resize", handleWidth);
+  });
+
   return (
     <div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <dl className="grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-2">
-          <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+          <div className="mx-auto flex max-w-xs flex-col ">
             SIBLING COLUMN1
-            <canvas id="myCanvas" width="200" height="100" style={{border:"1px solid #000000"}}>
-</canvas>
+            <canvas
+              id="myCanvas"
+              width={canvasWidth}
+              height="100"
+              style={{ border: "1px solid #000000" }}
+            ></canvas>
           </div>
-          <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-            SIBLING COLUMN2
-          </div>
+          <div className="mx-auto flex max-w-xs flex-col ">SIBLING COLUMN2</div>
         </dl>
       </div>
     </div>
